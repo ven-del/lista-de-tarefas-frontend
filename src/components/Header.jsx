fc5d091b4b888e25b8154398c2939e9f8551cbf9
Wendell Régis <r.wendell.regis@gmail.com>
2025-06-27 16:46:58 -0300

    feat: Criados Header e Footer
    Componentes criados:
    
    Header: tem a logo, o título (que vai ser alterado pra algo diânimco depois) e dois botões: login e cadastro.
    
    Footer: apenas a informação que foi criado por mim.

---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+const Header = () => {
+    return (
+      <header className="flex items-center justify-between bg-neutral-900 text-white p-4 border-b border-amber-100">
+        <img src="/assets/images/logo.png" alt="Logo do site Lista Dinâmica" />
+        <h1>Oi, eu sou o título!</h1>
+        <div>
+                <Link
+                    to="/cadastro"
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                >
+                    Cadastro
+                </Link>
+                <Link
+                    to="/login"
+                    className="bg-amber-100 hover:bg-amber-50 text-white font-bold py-2 px-4 rounded ml-2"
+                >
+                    Login
+                </Link>
+        </div>
+      </header>
+    );
+}
+ 
+export default Header;
