7d86b1bede1a5f9ed7faa37fed6abdac8d437451
Wendell Régis <r.wendell.regis@gmail.com>
2025-06-28 18:45:11 -0300

    fix: Header import
    Corrigido o import do Header no SiteLayout

--- src/layouts/SiteLayout.jsx
+++ src/layouts/SiteLayout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import Header from "../components/header";
+import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 const SiteLayout = () => {
