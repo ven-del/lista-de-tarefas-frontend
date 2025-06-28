102c8e60ebae4e04356ef61a218ba08037d8bb79
Wendell Régis <r.wendell.regis@gmail.com>
2025-06-28 18:41:19 -0300

    fix: imprt SiteLayout
    Corrigido o import do SiteLayout no Paths para o caminho correto.

--- src/routes/Paths.jsx
+++ src/routes/Paths.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { UserProvider } from "../contexts/UserContext";
 import Home from "../pages/Home";
-import SiteLayout from "../Layouts/SiteLayout";
+import SiteLayout from "../layouts/SiteLayout";
 import NotFound from "../pages/NotFound";
 import Register from "../pages/Register";
 import Login from "../pages/Login";
